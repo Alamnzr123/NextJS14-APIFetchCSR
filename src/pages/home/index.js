be37722c1aac3b3a5763f6a38bba18f78209d194
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2023-11-22 14:51:51 +0700

    first commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+
+const Home = () => {
+    const [data, setData] = React.useState();
+
+    async function handleSubmit() {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+        setData(res.data);
+    }
+    return (
+        <>
+            You can fetch Data Just Click this Button!
+            <br />
+            <button onClick={() => handleSubmit()}>Submit</button>
+            {console.log(data)}
+            {
+                // ? Use the safe navigation operator ( ?. ) to replace explicit, sequential checks for null references.
+                data?.map((item, index) => (
+                    <div style={{ color: 'white' }} key={index} >
+                        {`ID ${item.id}`}
+                        {`Name ${item.name}`}
+                    </div>
+                ))
+            }
+
+        </>
+    )
+}
+
+export default Home
